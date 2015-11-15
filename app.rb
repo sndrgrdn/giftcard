cc85ed40767219aaf8c8b48d23fb3940466cb399
Sander Tuin <sander@hackerone.com>
2015-11-15 11:30:03 +0100

    OOps

--- app.rb
+++ app.rb
@@ -34,7 +34,7 @@ post '/cards' do
     PIN: scnd_code
   }) if code && scnd_code && !req_url.empty?
   ## TODO: get value from response
-  params['card']['value'] = response
+  params['card']['value'] = response.to_d
   @card = Card.new(params[:card])
   if @card.save
     redirect 'cards'
